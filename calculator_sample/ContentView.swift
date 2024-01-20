6de1f5b4b979cd0a1e3b2627733b176eb5b510d6
Yoshikawa Fuma <k21rs139@st.kyusan-u.ac.jp>
2024-01-20 18:27:42 +0900

    Initial Commit

---
+++ calculator_sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  calculator_sample
+//
+//  Created by 吉川楓馬 on 2024/01/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ calculator_sample/calculator_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_sampleApp.swift
+//  calculator_sample
+//
+//  Created by 吉川楓馬 on 2024/01/20.
+//
+
+import SwiftUI
+
+@main
+struct calculator_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
